1ec3c6af2ca943f10b5c2b71578285944c3147cd
Evans Rutto <robbyevans001@gmail.com>
2022-06-07 21:12:06 +0300

    commit tree 53f5f1afe57bf9e6f101dd26e783243b7f377727

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(msg='roller-skate'){
+    return `This Saturday, I want to ${msg}!`
+};
+saturdayFun('bathe my dog');
+
+function mondayWork(message='go to the office'){
+    return `This Monday, I will ${message}.` 
+};
+mondayWork('work from home');
+
+function  wrapAdjective(msg='*'){
+    return function  (txt='special'){
+        return `You are ${msg}${txt}${msg}!`
+    }
+}
